9bab04d10144e9f17bfdf6300fb0e49ffa422beb
safa-abidi <safa.abidi99@gmail.com>
2022-04-25 22:00:52 +0100

    get todo by id

---
+++ data/db.js
@@ -0,0 +1,22 @@
+export const db = {
+    todos : [
+        {
+            id: "1",
+            name: 'sport',
+            content: 'do some push ups',
+            status: "DONE"
+        },
+        {
+            id: "2",
+            name: 'school',
+            content: 'revise nestjs',
+            status: "IN_PROGRESS"
+        },
+        {
+            id: "3",
+            name: 'school',
+            content: 'revise gQL',
+            status: "IN_PROGRESS"
+        },
+    ]
+}
---
+++ resolvers/Query.js
@@ -0,0 +1,16 @@
+import {db} from "../data/db.js";
+
+export const Query = {
+    hello: (parent, args, context, info) => {
+        console.log('parent : ', parent);
+        console.log('context : ', context);
+        console.log('info : ', info);
+        console.log('args : ', args);
+        return `Hello ${args.name || 'World'}`;
+    },
+    getTodo: (_, {id}) => {
+        return db.todos.find(
+            (todo) => todo.id === id
+        );
+    }
+}
